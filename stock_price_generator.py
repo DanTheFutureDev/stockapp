e42bd6dd3e706dd1926d883864ddbbf48db4b700
Daniel Young <dlyoung9@asu.edu>
2025-04-26 21:42:20 -0700

    New feature to show high and low stock price

--- stock_price_generator.py
+++ stock_price_generator.py
@@ -24,15 +24,32 @@ def update_stock_prices():
     from app import app  # Import app within the function to avoid circular import
     with app.app_context():
         stocks = Stock.query.all()
+        current_date = datetime.now().date()
+        
         for stock in stocks:
             new_price = generate_random_price(stock.current_price)
+
+            # Set opening price if it's a new day
+            if not stock.opening_price or not stock.last_updated or stock.last_updated.date() != current_date:
+
+                stock.opening_price = new_price
+                stock.high_price = new_price
+                stock.low_price = new_price
+            else:
+                if new_price > (stock.high_price or 0):
+                    stock.high_price = new_price
+                if new_price < (stock.low_price or new_price):
+                    stock.low_price = new_price
+
             logger.info(f"Updating {stock.ticker} from {stock.current_price} to {new_price}")
             stock.current_price = new_price
+            stock.last_updated = datetime.now()  # You can add this column to track update time
+
             stock_history = StockHistory(stock_id=stock.id, price=new_price)
             db.session.add(stock_history)
+
         db.session.commit()
         logger.info(f"Stock prices updated at {datetime.now()}")
-    logger.info("Stock prices updated.")
 
 if __name__ == '__main__':
     update_stock_prices()
